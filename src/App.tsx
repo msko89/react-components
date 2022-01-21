455694e6273eda516322bdda7b8487c4abce6bd7
msko <kms89123@gmail.com>
2022-01-22 00:08:59 +0900

    Carousel Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import styled from '@emotion/styled/macro';
-import Skeleton from './components/Skeleton';
-
-const Base = styled.div`
-  display: grid;
-  width: 100%;
-  grid-template-columns: repeat(5, 1fr);
-  column-gap: 12px;
-  row-gap: 24px;
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  box-shadow: rgb(0 0 0 / 4%) 0px 4px 16px 0px;
-  border-radius: 4px;
-`;
-
-const ImageWrapper = styled.div`
-  width: 100%;
-  margin: 0 auto;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
-
-const Info = styled.div`
-  padding: 1rem;
-  display: flex;
-  flex-direction: column;
-  flex: 1 1 0%;
-`;
-
-const Title = styled.h4`
-  margin: 0;
-  padding: 0;
-  font-size: 24px;
-`;
-
-const Description = styled.p`
-  margin: 8px 0 0 0;
-  padding: 0;
-  font-size: 16px;
-`;
-
-const Placeholder: React.FC = () => (
-  <Container>
-    <ImageWrapper>
-      <Skeleton width={320} height={220} />
-    </ImageWrapper>
-    <Info>
-      <Skeleton width={150} height={29} rounded />
-      <div style={{ height: '8px' }} />
-      <Skeleton width={200} height={19} rounded />
-    </Info>
-  </Container>
-);
-
-const Item: React.FC = () => {
-  return (
-    <Container>
-      <ImageWrapper>
-        <Image src='https://img1.daumcdn.net/thumb/R750x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbc4USA%2Fbtq69WpP9fO%2FVuOSUpuTidKClnqKzUri90%2Fimg.jpg' />
-      </ImageWrapper>
-      <Info>
-        <Title>Skeleton Title</Title>
-        <Description>Skeleton Description</Description>
-      </Info>
-    </Container>
-  );
-};
+import React from 'react';
+import Carousel from './components/Carousel';
+// import SkeletonPage from './components/Skeleton';
 
 function App() {
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 2000);
-  }, []);
-
   return (
-    <Base>
-      {loading
-        ? Array.from({ length: 25 }).map((_, idx) => <Placeholder key={idx} />)
-        : Array.from({ length: 25 }).map((_, idx) => <Item key={idx} />)}
-    </Base>
+    <>
+      {/* <SkeletonPage /> */}
+      <Carousel />
+    </>
   );
 }
 
--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled/macro';
 import { keyframes, css } from '@emotion/react';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 interface Props {
   width?: number;
@@ -76,4 +76,92 @@ const Skeleton: React.FC<Props> = ({
   );
 };
 
-export default Skeleton;
+const BasePage = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(5, 1fr);
+  column-gap: 12px;
+  row-gap: 24px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-shadow: rgb(0 0 0 / 4%) 0px 4px 16px 0px;
+  border-radius: 4px;
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+  margin: 0 auto;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Info = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 0%;
+`;
+
+const Title = styled.h4`
+  margin: 0;
+  padding: 0;
+  font-size: 24px;
+`;
+
+const Description = styled.p`
+  margin: 8px 0 0 0;
+  padding: 0;
+  font-size: 16px;
+`;
+
+const Placeholder: React.FC = () => (
+  <Container>
+    <ImageWrapper>
+      <Skeleton width={320} height={220} />
+    </ImageWrapper>
+    <Info>
+      <Skeleton width={150} height={29} rounded />
+      <div style={{ height: '8px' }} />
+      <Skeleton width={200} height={19} rounded />
+    </Info>
+  </Container>
+);
+
+const Item: React.FC = () => {
+  return (
+    <Container>
+      <ImageWrapper>
+        <Image src='https://img1.daumcdn.net/thumb/R750x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbc4USA%2Fbtq69WpP9fO%2FVuOSUpuTidKClnqKzUri90%2Fimg.jpg' />
+      </ImageWrapper>
+      <Info>
+        <Title>Skeleton Title</Title>
+        <Description>Skeleton Description</Description>
+      </Info>
+    </Container>
+  );
+};
+
+const SkeletonPage: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000);
+  }, []);
+
+  return (
+    <BasePage>
+      {loading
+        ? Array.from({ length: 25 }).map((_, idx) => <Placeholder key={idx} />)
+        : Array.from({ length: 25 }).map((_, idx) => <Item key={idx} />)}
+    </BasePage>
+  );
+};
+
+export default SkeletonPage;
